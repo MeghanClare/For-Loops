41b4ad4e146c2241f130e3dc833fd58edc0350ed
MegHanSolo <meghanlangill@gmail.com>
2016-04-27 12:42:16 -0700

    refactor for clarity

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -11,18 +11,29 @@
 //	}
 // }
 
-for (var number = 100; number >= 0; number = number - 1) {
-	if (number % 5 === 0) {
-		if (number % 3 === 0) {
-			console.log('FizzBuzz');
-		} else {
-			console.log('Fizz'); }
-	} else { 
-		if (number % 3 === 0) {
-			console.log('Buzz'); 
-		}
-		else {
-			console.log(number);
-		}
+// for (var number = 1; number <= 100; number = number + 1) {
+// 	if (number % 5 === 0) {
+// 		if (number % 3 === 0) {
+// 			console.log('FizzBuzz');
+// 		} else {
+// 			console.log('Fizz'); }
+// 	} else { 
+// 		if (number % 3 === 0) {
+// 			console.log('Buzz'); 
+// 		}
+// 		else {
+// 			console.log(number);
+// 		}
+// 	}
+// }
+for (var number = 1; number <= 100; number = number +1) {
+	if (number % 5 === 0 && number % 3 === 0) {
+		console.log('FizzBuzz') 
+	} else if (number % 3 === 0) {
+		console.log('Fizz')
+	} else if (number % 5 === 0) {
+		console.log('Buzz'); 
+	} else {
+		console.log(number)
 	}
 }
--- triangle.js
+++ triangle.js
@@ -1,6 +1,6 @@
 // Write a loop that makes seven calls to console.log to output the following
-// its a fucking triangle
-// .length is a property of all strings that can find length
+// its a fucking triangle 
+// .length is a property of all strings that can find length 
 
 //for (var text = '#'; text.length <= 7; text = text + '#') {
 //	console.log(text); 
@@ -8,5 +8,6 @@
 
 // print an 8x8 grid 
 
-for (var text = '#'; text >= .length; text = text + ' ');
-	if (text)
+for (var text = '#'; text.length < 5; text = text + "#"){
+	console.log(text)
+}
